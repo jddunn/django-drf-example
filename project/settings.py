5d956f3ce2abff7665a690605d9486a0ce9accda
jddunn <johnnyfived@protonmail.com>
2026-01-05 10:06:42 -0800

    update deps

--- project/settings.py
+++ project/settings.py
@@ -15,9 +15,8 @@ import os.path
 
 import example_app
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BASE_DIR / 'subdir'
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
 
